ccdaf14e10d189509890a4f40770f4ec721c6cb2
Miguel Angel <miguelangelteletor@yahoo.es>
2020-05-13 01:29:02 +0200

    added the component where i puto some sections scroll on chintias films

---
+++ src/components/scroll/Scroll.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Scroll.scss'
+
+const  Scroll = () => {
+       return (
+              <div className="scrollCont">
+              <div className="firstSection">
+
+              </div>
+                     
+              </div>
+       )
+}
+
+export default Scroll
--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -1,12 +1,24 @@
 import React from 'react'
 import  './Search.scss'
 
+import { Input } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+
+
+
+
 const Search = () => {
        return (
               <div>
-              <h1>Hello world</h1>
-                     
+              <h1>All films and series would you like <br></br> and little more</h1>
+              <p>Enjoy anywhere would you like. leave your suscription any time</p>
+             
+                    
+                   
+                      
               </div>
+
+
        )
 }
 
