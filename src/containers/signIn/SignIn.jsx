21362adcffede564e74490c80b91b0ce00e68d83
Miguel Angel <miguelangelteletor@yahoo.es>
2020-05-17 00:50:01 +0200

    adding some improves for the signin page

--- src/containers/signIn/SignIn.jsx
+++ src/containers/signIn/SignIn.jsx
@@ -40,7 +40,7 @@ export class SignIn extends Component {
           },
         ]}
       >
-        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+        <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="email" />
       </Form.Item>
       <Form.Item
         name="password"
@@ -57,21 +57,13 @@ export class SignIn extends Component {
           placeholder="Password"
         />
       </Form.Item>
-      <Form.Item>
-        <Form.Item name="remember" valuePropName="checked" noStyle>
-          <Checkbox>Remember me</Checkbox>
-        </Form.Item>
-
-        <a className="login-form-forgot" href="">
-          Forgot password
-        </a>
-      </Form.Item>
+     
 
       <Form.Item>
-        <Button type="primary" htmlType="submit" className="login-form-button">
+        <Button  htmlType="submit" className="login-form-button">
           Log in
         </Button>
-        Or <a href="">register now!</a>
+        Or <NavLink to="/home"><a >register now!</a></NavLink>
       </Form.Item>
     </Form>
                          </div>
