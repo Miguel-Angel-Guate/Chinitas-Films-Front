6b9f4a19a96bae250c8b67418c84e3f1d9937f58
Miguel Angel <miguelangelteletor@yahoo.es>
2020-05-21 17:08:28 +0200

    the first endpoint populars is working

--- src/containerOfChaposHome/Populars/PopularsFilms.jsx
+++ src/containerOfChaposHome/Populars/PopularsFilms.jsx
@@ -1,27 +1,29 @@
 import React, { useEffect } from 'react';
 import './PopularsFilms.scss'
 import { connect } from 'react-redux';
-import { populars } from './../../redux/actions/Populars'
+import { getPopulars } from './../../redux/actions/Populars'
 
 
-const PopularsFilms = (props) => {
+ const PopularsFilms = ({populars}) => {
        useEffect(() => {
               console.log()
-              populars()
+              getPopulars()
                   .catch(console.error)
           }, []);
           
 
        return (
+              <div className="popularsFilms">
+            {populars?.map(popular=> 
+                <img src={'http://image.tmdb.org/t/p/w185'+ popular.poster_path} alt=""/>
+                
+            )}
+        </div>
               
-
-              
-              <div></div>
-
-       
                      
               
        )
+
 }
- 
-export default PopularsFilms
+const mapStateToProps  = ({Populars}) => ({populars:Populars?.populars})
+export default connect(mapStateToProps)(PopularsFilms)
--- src/redux/actions/Populars.js
+++ src/redux/actions/Populars.js
@@ -1,7 +1,7 @@
 import store from '../store';
 import axios from 'axios';
 
-export const populars = async (value) => {
+export const getPopulars = async (value) => {
     const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=cea68b520beecac6718820e4ac576c3a&language=en-EN&page=1`);
     store.dispatch({
         type: 'POPULARS',
